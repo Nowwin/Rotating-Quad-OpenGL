1b3970b56107b99788d0f972b19d180aaf794556
Abhijeet Chowdhury <abhijeetchowdhury1993@gmail.com>
2023-08-28 07:19:00 +0530

    Adding shaders

---
+++ shaders/vert.glsl
@@ -0,0 +1,19 @@
+#version 410 core
+
+
+layout(location=0) in vec3 position;
+layout(location=1) in vec3 vertexColors;
+
+uniform mat4 u_ModelMatrix;
+uniform mat4 u_PerspectiveMatrix;
+
+out vec3 v_vertexColors;
+
+void main()
+{
+    v_vertexColors = vertexColors;
+
+    vec4 newPosition = u_PerspectiveMatrix * u_ModelMatrix * vec4(position, 1.0f);
+    
+    gl_Position = vec4(newPosition.x, newPosition.y, newPosition.z, newPosition.w);
+}
